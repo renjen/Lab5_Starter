34be10324abb820b5c5a4d0e732c3a805c1e3813
renjen <renjende@gmail.com>
2025-05-05 13:35:49 -0700

    Implement explore.js speech synthesis

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Lab 5 - Starter
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
 Renee Jenette Dhanaraj
+GitHub Username: renjen
+GitHub Pages Link: https://renjen.github.io/Lab5_Starter/expose.html
+
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,41 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.getElementById('voice-select');
+  const textInput = document.getElementById('text-to-speak');
+  const talkButton = document.querySelector('button');
+  const faceImage = document.querySelector('img');
+
+  let voices = [];
+
+  function populateVoices() {
+    voices = synth.getVoices();
+
+    voices.forEach(voice => {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.value = voice.name;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  populateVoices();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoices;
+  }
+
+  talkButton.addEventListener('click', () => {
+    const utterance = new SpeechSynthesisUtterance(textInput.value);
+    const selectedVoiceName = voiceSelect.value;
+
+    utterance.voice = voices.find(voice => voice.name === selectedVoiceName);
+
+    faceImage.src = 'assets/images/smiling-open.png';
+    synth.speak(utterance);
+
+    utterance.onend = () => {
+      faceImage.src = 'assets/images/smiling.png';
+    };
+  });
 }
