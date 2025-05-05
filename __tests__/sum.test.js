c5c800b6840da3d163f3a141cf40b095f6bff24c
renjen <renjende@gmail.com>
2025-05-05 15:15:40 -0700

    testing branch: should be wrong

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,4 +2,11 @@
 
 test('adds 1 + 2 to equal 3', () => {
   // TODO
+  expect(1 + 2).toBe(3);
+});
+
+import { sum } from '../code-to-unit-test/sum.js';
+
+test('sum() function adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
 });
