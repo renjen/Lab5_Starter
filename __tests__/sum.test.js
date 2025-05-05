12bd4cff3e599bfb4280f341a302e94cc0bcbd41
renjen <renjende@gmail.com>
2025-05-05 15:30:54 -0700

    Add intentional failing test

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,12 +1,16 @@
 // sum.test.js
 
+import { sum } from '../code-to-unit-test/sum.js';
+
 test('adds 1 + 2 to equal 3', () => {
-  // TODO
   expect(1 + 2).toBe(3);
 });
 
-import { sum } from '../code-to-unit-test/sum.js';
-
 test('sum() function adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+// INTENTIONALLY FAILING TEST
+test('adds 1 + 2 to equal 4', () => {
+  expect(1 + 2).toBe(4);  //  wrong on purpose
 });
